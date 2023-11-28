5c8b364b82ec0212971abd1da5abe1ad3e668321
R1013-T <nosuke1013@icloud.com>
2023-11-28 14:15:30 +0900

    add layout

---
+++ app/blog/page.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'ブログ',
+}
+
+export default function Page() {
+  return (
+    <article className="border border-green-400 h-full p-4 mx-auto max-w-6xl overflow-hidden">
+      <div className="h-auto w-full border border-blue-500 hidden lg:grid grid-cols-4 gap-4">
+        <div className="border border-white bg-white/40 rounded-md opacity-0 showBottomTop h-52"></div>
+        <div className="border border-white bg-white/40 rounded-md opacity-0 showBottomTop h-52"></div>
+        <div className="border border-white bg-white/40 rounded-md opacity-0 showBottomTop h-52"></div>
+        <div className="border border-white bg-white/40 rounded-md opacity-0 showBottomTop h-52"></div>
+        <div className="border border-white bg-white/40 rounded-md opacity-0 showBottomTop h-52"></div>
+      </div>
+    </article>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'black-1': '#2C2C2C',
+        'black-2': '#495057',
+        'white-1': '#FAFAFA',
+        'white-2': '#F8FAFC',
+        'white-3': '#E7F7F9',
+        'white-4': '#EDEDED',
+      },
     },
   },
   plugins: [],
