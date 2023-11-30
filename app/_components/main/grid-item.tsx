33d681ab161b081e85bcd9e1116ea1e88da71d5a
R1013-T <nosuke1013@icloud.com>
2023-11-30 22:12:39 +0900

    change grid style round

--- app/_components/main/grid-item.tsx
+++ app/_components/main/grid-item.tsx
@@ -13,7 +13,7 @@ export default function GridItem({
 }) {
   return (
     <div
-      className={`col-span-${col} row-span-${row} bg-white-1/60 rounded-md opacity-0 cursor-pointer transition-colors hover:bg-white-1/70  ${animation} ${
+      className={`col-span-${col} row-span-${row} bg-white-1/60 rounded-xl overflow-hidden opacity-0 cursor-pointer transition-colors hover:bg-white-1/70  ${animation} ${
         disabled && 'pointer-events-none'
       }`}
     >
