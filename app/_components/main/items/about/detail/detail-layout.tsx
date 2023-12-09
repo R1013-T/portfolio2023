5b9b58f4bdbef76c07ff10ebc204b424d428c18b
R1013-T <nosuke1013@icloud.com>
2023-12-09 17:36:37 +0900

    change about detail border color

--- app/_components/main/items/about/detail/detail-layout.tsx
+++ app/_components/main/items/about/detail/detail-layout.tsx
@@ -12,7 +12,7 @@ export default function AboutDetailLayout({
       {name && (
         <div className="flex items-center mb-2">
           <h2 className="text-2xl font-bold whitespace-nowrap pr-3">{name}</h2>
-          <div className="border-b-2 border-gray-300 w-full"></div>
+          <div className="border-b-2 border-white-2 w-full"></div>
         </div>
       )}
       <div className="p-1">{children}</div>
